b4c68c8ff85071d482dcf995dbd10be5fcf877b4
1fatm <mariemedial@gmail.com>
2024-06-21 15:04:43 +0000

    nouveau

--- script.js
+++ script.js
@@ -1,131 +1,187 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AfficherTâche();
 
-        function AfficherTâche() {
-            let taskList = document.getElementById("task-list");
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let boutonAjouter = document.getElementById("b1");
+    boutonAjouter.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+});
 
-            if (tasks.length === 0) {
-                taskList.innerHTML = "<p>Aucune tâche à afficher.</p>";
-                return;
-            }
-            tasks.forEach(task => {
-                let taskCard = document.createElement("div");
-                taskCard.className = "task-card";
-                let statusBtnClass;
-                switch (getTaskStatus(task)) {
-                    case "En cours":
-                        statusBtnClass = "en-cours";
-                        break;
-                    case "Terminée":
-                        statusBtnClass = "terminée";
-                        break;
-                    case "À venir":
-                        statusBtnClass = "à-venir";
-                        break;
-                    default:
-                        statusBtnClass = "";
-                }
+function AfficherTâche() {
+    let taskList = document.getElementById("task-list");
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-                taskCard.innerHTML = `
-                    <div class="task-card">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div>
-                                <div class="task-title">${task.titre}</div>
-                                <span><strong>Date de début:</strong> ${task.dateDebut}</span>
-                                <span><strong>Date de fin:</strong> ${task.dateFin}</span>
-                            </div>
-                            <div class="task-actions">
-                                <button class="status-btn ${statusBtnClass}">${getTaskStatus(task)}</button>
-                                <button class="btn btn-info btn-sm" onclick="ouvrirModifierTache('${task.titre}')"><i class="fas fa-pencil-alt"></i></button>
-                                <button class="btn btn-danger btn-sm" onclick="supprimerTache('${task.titre}')"><i class="fas fa-trash"></i></button>
-                                <button class="btn btn-primary btn-sm" onclick="voirDetails('${task.titre}')"><i class="fas fa-eye"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                `;
+    taskList.innerHTML = "";
 
-                taskList.appendChild(taskCard);
-            });
+    if (tasks.length === 0) {
+        taskList.innerHTML = "<p>Aucune tâche à afficher.</p>";
+        return;
+    }
 
-            updateTaskStatistics();
-        }
-        function updateTaskStatistics() {
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-            let totalTasks = tasks.length;
-            let completedTasks = tasks.filter(task => task.status === "Terminée").length;
-            let ongoingTasks = tasks.filter(task => task.status === "En cours").length;
-            let upcomingTasks = tasks.filter(task => getTaskStatus(task) === "À venir").length;
+    tasks.forEach((task) => {
+        let taskCard = createTaskCard(task);
+        taskList.appendChild(taskCard);
+    });
+
+    updateTaskStatistics();
+}
+
+function createTaskCard(task) {
+    let taskCard = document.createElement("div");
+    taskCard.className = "task-card";
+    let statusBtnClass = getStatusButtonClass(task);
+
+    taskCard.innerHTML = `
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <div class="task-title">${task.titre}</div>
+                <span><strong>Date de début:</strong> ${task.dateDebut}</span>
+                <span><strong>Date de fin:</strong> ${task.dateFin}</span>
+                <span><strong>Statut:</strong> ${getTaskStatus(task)}</span> <!-- Affiche le statut -->
+            </div>
+            <div class="task-actions">
+                 <button class="btn ${statusBtnClass} btn-sm" >${getTaskStatus(task)}</button> 
+                <button class="btn btn-info btn-sm" onclick="ouvrirModifierTache('${task.titre}')"><i class="fas fa-pencil-alt"></i></button>
+
+                <button class="btn btn-danger btn-sm" onclick="supprimerTache('${task.titre}')"><i class="fas fa-trash"></i></button>
+                <button class="btn btn-primary btn-sm" onclick="voirDetails('${task.titre}')"><i class="fas fa-eye"></i></button>
+               
+            </div>
+        </div>
+        <hr>
+    `;
+
+    return taskCard;
+}
+
+function ouvrirModifierTache(titre) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let task = tasks.find((t) => t.titre === titre);
+
+    if (task) {
+        document.getElementById("editTaskTitle").value = task.titre;
+        document.getElementById("editTaskDescription").value = task.description;
+        document.getElementById("editTaskStartDate").value = task.dateDebut;
+        document.getElementById("editTaskEndDate").value = task.dateFin;
+
+        let taskStatus = getTaskStatus(task);
+        let startDateField = document.getElementById("editTaskStartDate");
+        let endDateField = document.getElementById("editTaskEndDate");
+
+        let today = new Date().toISOString().split('T')[0]; 
+
         
-            document.getElementById("total-tasks").textContent = totalTasks;
-            document.getElementById("completed-tasks").textContent = completedTasks;
-            document.getElementById("ongoing-tasks").textContent = ongoingTasks;
-            document.getElementById("upcoming-tasks").textContent = upcomingTasks;
-        }
-        
+        startDateField.setAttribute("min", today);
+        endDateField.setAttribute("min", task.dateDebut);
 
-        function ouvrirModifierTache(titre) {
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-            let task = tasks.find(t => t.titre === titre);
-
-            if (task) {
-                document.getElementById("editTaskTitle").value = task.titre;
-                document.getElementById("editTaskDescription").value = task.description;
-                document.getElementById("editTaskStartDate").value = task.dateDebut;
-                document.getElementById("editTaskEndDate").value = task.dateFin;
-
-
-                document.getElementById("editTaskForm").onsubmit = function(event) {
-                    event.preventDefault();
-                    modifierTache(task);
-                
-                };
-            }
+        if (taskStatus === "À venir") {
+            startDateField.removeAttribute("disabled");
+            startDateField.setAttribute("min", today); 
+            endDateField.removeAttribute("disabled");
+            endDateField.setAttribute("min", today); 
+        } else if (taskStatus === "En cours") {
+            startDateField.setAttribute("disabled", "true");
+            endDateField.removeAttribute("disabled");
+            endDateField.setAttribute("min", today); 
+        } else if (taskStatus === "Terminée") {
+            startDateField.setAttribute("disabled", "true");
+            endDateField.setAttribute("disabled", "true");
         }
 
-        function modifierTache(task) {
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        $('#editTaskModal').modal('show');
+        let modal = document.getElementById("editTaskModal");
+        modal.addEventListener("submit", function (event) {
+            event.preventDefault(); 
+            modifierTache(task);
 
-            task.titre = document.getElementById("editTaskTitle").value;
-            task.description = document.getElementById("editTaskDescription").value;
-            task.dateDebut = document.getElementById("editTaskStartDate").value;
-            task.dateFin = document.getElementById("editTaskEndDate").value;
-          
+            $('#editTaskModal').modal('hide');
+        }, { once: true }); 
+    }
+}
 
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            AfficherTâche();
+
+function modifierTache(task) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let index = tasks.findIndex((t) => t.titre === task.titre);
+
+    if (index !== -1) {
+        let taskStatus = getTaskStatus(task);
+
+        tasks[index].titre = document.getElementById("editTaskTitle").value;
+        tasks[index].description = document.getElementById("editTaskDescription").value;
+
+        if (taskStatus === "À venir") {
+            tasks[index].dateDebut = document.getElementById("editTaskStartDate").value;
+            tasks[index].dateFin = document.getElementById("editTaskEndDate").value;
+        } else if (taskStatus === "En cours") {
+            tasks[index].dateFin = document.getElementById("editTaskEndDate").value;
         }
 
-        function supprimerTache(titre) {
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-            tasks = tasks.filter(task => task.titre !== titre);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        AfficherTâche();
+        $('#editTaskModal').modal('hide');
+    }
+}
 
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            AfficherTâche();
-        }
 
-        function voirDetails(titre) {
-            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-            let task = tasks.find(t => t.titre === titre);
+function supprimerTache(titre) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks = tasks.filter((task) => task.titre !== titre);
 
-            if (task) {
-                document.getElementById("taskDetailsContent").innerText = `
-                    Titre: ${task.titre}
-                    Description: ${task.description}
-                    Date de début: ${task.dateDebut}
-                    Date de fin: ${task.dateFin}
-                    Statut: ${task.status}
-                `;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    AfficherTâche();
+}
 
-            }
-        }
-        function getTaskStatus(task) {
-            let today = new Date().toISOString().split('T')[0]; 
-            if (task.dateDebut <= today && task.dateFin >= today) {
-                return "En cours";
-            } else if (task.dateFin < today) {
-                return "Terminée";
-            } else {
-                return "À venir";
-            }
-        }
-        
-        document.addEventListener("DOMContentLoaded", AfficherTâche);
+function voirDetails(titre) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let task = tasks.find((t) => t.titre === titre);
+
+    if (task) {
+        document.getElementById("taskDetailsContent").innerHTML = `
+            <p><strong>Titre:</strong> ${task.titre}</p>
+            <p><strong>Description:</strong> ${task.description}</p>
+            <p><strong>Date de début:</strong> ${task.dateDebut}</p>
+            <p><strong>Date de fin:</strong> ${task.dateFin}</p>
+            <p><strong>Statut:</strong> ${getTaskStatus(task)}</p>
+        `;
+
+        $('#taskDetailsModal').modal('show');
+    }
+}
+
+function getTaskStatus(task) {
+    let today = new Date().toISOString().split('T')[0];
+    if (task.dateDebut <= today && task.dateFin >= today) {
+        return "En cours";
+    } else if (task.dateFin < today) {
+        return "Terminée";
+    } else {
+        return "À venir";
+    }
+}
+
+function getStatusButtonClass(task) {
+    switch (getTaskStatus(task)) {
+        case "En cours":
+            return "btn-warning";
+        case "Terminée":
+            return "btn-success";
+        case "À venir":
+            return "btn-info";
+        default:
+            return "btn-secondary";
+    }
+}
+
+function updateTaskStatistics() {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let totalTasks = tasks.length;
+    let completedTasks = tasks.filter((task) => getTaskStatus(task) === "Terminée").length;
+    let ongoingTasks = tasks.filter((task) => getTaskStatus(task) === "En cours").length;
+    let upcomingTasks = tasks.filter((task) => getTaskStatus(task) === "À venir").length;
+
+    document.getElementById("total-tasks").textContent = totalTasks;
+    document.getElementById("completed-tasks").textContent = completedTasks;
+    document.getElementById("ongoing-tasks").textContent = ongoingTasks;
+    document.getElementById("upcoming-tasks").textContent = upcomingTasks;
+}
--- script2.js
+++ script2.js
@@ -1,74 +1,90 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addTaskModal = new bootstrap.Modal(document.getElementById("addTaskModal"));
+  const taskForm = document.getElementById("task-form");
+  const successMessage = document.getElementById("success-message");
+  const titreInput = document.getElementById("titre");
+  const descriptionInput = document.getElementById("description");
+  const dateDebutInput = document.getElementById("dateDebut");
+  const dateFinInput = document.getElementById("dateFin");
+  const titreErreur = document.getElementById("titre-erreur");
+  const descriptionErreur = document.getElementById("description-erreur");
+  const dateDebutErreur = document.getElementById("date-debut-erreur");
+  const dateFinErreur = document.getElementById("date-fin-erreur");
+  const annuler = document.getElementById("annuler");
 
-        let taskForm = document.getElementById("task-form");
-        let titre = document.getElementById("titre");
-        let description = document.getElementById("description");
-        let dateDebut = document.getElementById("dateDebut");
-        let dateFin = document.getElementById("dateFin");
-        let titreErreur = document.getElementById("titre-erreur");
-        let descriptionErreur = document.getElementById("description-erreur");
-        let dateDebutErreur = document.getElementById("date-debut-erreur");
-        let dateFinErreur = document.getElementById("date-fin-erreur");
-        let successMessage = document.getElementById("success-message");
 
-        let aujourdHui = new Date().toISOString().split('T')[0];
-        dateDebut.setAttribute("min", aujourdHui);
-        dateFin.setAttribute("min", aujourdHui);
+  annuler.addEventListener("click", function() {
+    window.location.href = "pageDeredirection.html";
+  });
 
-        document.getElementById("b1").addEventListener("click", function() {
-            window.location.href = "pageDeredirection.html";
-        });
+  
+  const today = new Date().toISOString().split('T')[0];
+  dateDebutInput.setAttribute("min", today);
+  dateFinInput.setAttribute("min", today);
 
-        taskForm.addEventListener("submit", function(event) {
-            event.preventDefault();
+  addTaskModal.show();
 
-            titreErreur.innerHTML = "";
-            descriptionErreur.innerHTML = "";
-            dateDebutErreur.innerHTML = "";
-            dateFinErreur.innerHTML = "";
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-            let isValid = true;
+    titreErreur.innerHTML = "";
+    descriptionErreur.innerHTML = "";
+    dateDebutErreur.innerHTML = "";
+    dateFinErreur.innerHTML = "";
 
-            if (!titre.value) {
-                titreErreur.innerHTML = "Veuillez entrer un titre";
-                isValid = false;
-            }
-            if (!description.value) {
-                descriptionErreur.innerHTML = "Veuillez entrer une description";
-                isValid = false;
-            }
-            if (!dateDebut.value) {
-                dateDebutErreur.innerHTML = "Veuillez entrer une date de début";
-                isValid = false;
-            }
-            if (!dateFin.value) {
-                dateFinErreur.innerHTML = "Veuillez entrer une date de fin";
-                isValid = false;
-            }
-            if (dateDebut.value == dateFin.value) {
-                dateFinErreur.innerHTML = "La date de fin doit être supérieure à la date de début";
-                isValid = false;
-            }
-            if (dateFin.value && dateDebut.value && dateFin.value < dateDebut.value) {
-                dateFinErreur.innerHTML = "La date de fin doit être supérieure à la date de début";
-                isValid = false;
-            }
+    let isValid = true;
 
-            if (isValid) {
-                let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-                let newTask = {
-                    titre: titre.value,
-                    description: description.value,
-                    dateDebut: dateDebut.value,
-                    dateFin: dateFin.value,
-                    status: "En cours"
-                };
-                tasks.push(newTask);
-                localStorage.setItem('tasks', JSON.stringify(tasks));
-                successMessage.style.display = "block";
-                setTimeout(function() {
-                    successMessage.style.display = "none";
-                }, 3000);
-                taskForm.reset();
-            }
-        });
-        
+    if (!titreInput.value) {
+      titreErreur.innerHTML = "Veuillez entrer un titre";
+      isValid = false;
+    }
+    if (!descriptionInput.value) {
+      descriptionErreur.innerHTML = "Veuillez entrer une description";
+      isValid = false;
+    }
+    if (!dateDebutInput.value) {
+      dateDebutErreur.innerHTML = "Veuillez entrer une date de début";
+      isValid = false;
+    }
+    if (!dateFinInput.value) {
+      dateFinErreur.innerHTML = "Veuillez entrer une date de fin";
+      isValid = false;
+    }
+    if (dateDebutInput.value === dateFinInput.value) {
+      dateFinErreur.innerHTML = "La date de fin doit être supérieure à la date de début";
+      isValid = false;
+    }
+    if (
+      dateFinInput.value &&
+      dateDebutInput.value &&
+      new Date(dateFinInput.value) <= new Date(dateDebutInput.value)
+    ) {
+      dateFinErreur.innerHTML = "La date de fin doit être supérieure à la date de début";
+      isValid = false;
+    }
+
+    if (isValid) {
+      let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+      let newTask = {
+        titre: titreInput.value,
+        description: descriptionInput.value,
+        dateDebut: dateDebutInput.value,
+        dateFin: dateFinInput.value,
+        status: "En cours",
+      };
+      tasks.push(newTask);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      successMessage.style.display = "block";
+      setTimeout(function () {
+        successMessage.style.display = "none";
+      }, 3000);
+      window.location.href = "pageDeredirection.html";
+      taskForm.reset();
+      addTaskModal.hide();
+    }
+  });
+
+  dateDebutInput.addEventListener("change", function () {
+    dateFinInput.setAttribute("min", dateDebutInput.value);
+  });
+});
